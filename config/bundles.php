39a787cba71ec33fe1e17dca991ecb99aff74d50
Jani Tarvainen <janit@iki.fi>
2017-10-20 18:16:47 +0300

    Updated webserver install

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
